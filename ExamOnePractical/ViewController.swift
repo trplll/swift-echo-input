d54779a682f44d03108462a82adef9cd68bd6946
Student <student@students-mac-29.local>
2017-03-04 03:28:28 -0600

    Initial Commit

---
+++ ExamOnePractical/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ExamOnePractical
+//
+//  Created by Student on 3/4/17.
+//  Copyright © 2017 Luke Lauerman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
