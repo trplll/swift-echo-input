440f1b551da51cdcff044fd6c994e0e9e3458bcb
Student <student@students-mac-29.fvtc.edu>
2017-03-12 00:59:56 -0600

    initial commit for exam one practical

--- ExamOnePractical/ViewController.swift
+++ ExamOnePractical/ViewController.swift
@@ -9,17 +9,34 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    
+    //create variables to connect to storyboard outlets
+    @IBOutlet weak var lblDisplayInput: UILabel!
+    @IBOutlet weak var txtUserInput: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        lblDisplayInput.text=" "
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+   
+    // btnEchoUserInput function is called by the action configured for storyboards echo button
+    @IBAction func btnEchoUserInput(sender: AnyObject) ->Void{
+        //asign txtUserInputs text property value to lblDisplayInputs Text property
+        lblDisplayInput.text=txtUserInput.text
+    }
+    // btnClearUserInput function is called by the action configured for storyboards clear button
+    @IBAction func btnClearUserInput(sender: AnyObject) ->Void {
+        //clear lblDipslayInputs text property
+        lblDisplayInput.text=" "
+    }
+    
+    
 }
 
